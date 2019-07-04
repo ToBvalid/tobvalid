e66c4dface501f131d754479d696364e8cd4c555
kavehb <kavehb@risk.az>
2019-07-04 17:43:00 +0400

    silverman finished

--- src/silverman.py
+++ src/silverman.py
@@ -14,31 +14,31 @@ import matplotlib.pyplot as plt
 from scipy import stats
 import scipy.optimize as sco
 import scipy.integrate as integr
-def kernel(datum, mu, sigma):
-    u = (datum - mu) / abs(sigma)
-    y = (1 / (sqrt(2 * pi) * abs(sigma))) * exp(-u * u / 2)
+def kernel(datum, mu, h):
+    u = (datum - mu) / abs(h)
+    y = (1 / (sqrt(2 * pi) * abs(h))) * exp(-u * u / 2)
     return y
 
-def approx(data, sigma):
-    return lambda x: sum([kernel(x, d, sigma) for d in data])/(len(data))
+def approx(data, h):
+    return lambda x: sum([kernel(x, d, h) for d in data])/(len(data))
 
-def mapprox(data, sigma):
-    return lambda x: sum([-1*kernel(x, d, sigma) for d in data])/(len(data))
+def mapprox(data, h):
+    return lambda x: sum([-1*kernel(x, d, h) for d in data])/(len(data))
 
-def dapprox(data, sigma):
-    return lambda x: sum([(-(x-d)/(sigma**2))*kernel(x, d, sigma) for d in data])/(len(data))
+def dapprox(data, h):
+    return lambda x: sum([(-(x-d)/(h**2))*kernel(x, d, h) for d in data])/(len(data))
 
-def ndapprox(data, sigma):
-    return lambda x: -sum([(-(x-d)/(sigma**2))*kernel(x, d, sigma) for d in data])/(len(data))
+def ndapprox(data, h):
+    return lambda x: -sum([(-(x-d)/(h**2))*kernel(x, d, h) for d in data])/(len(data))
 
-def approxy(data, sigma):
-    return lambda y, x: sum([(-(x-d)/(sigma**2))*kernel(x, d, sigma) for d in data])/(len(data))
+def approxy(data, h):
+    return lambda y, x: sum([(-(x-d)/(h**2))*kernel(x, d, h) for d in data])/(len(data))
 
 
-def minimize(data, sigma, iter = 5, x_tol = 0.00001):    
+def minimize(data, h, iter = 5, x_tol = 0.00001):    
     result = list()
-    df = ndapprox(data, sigma)
-    fm = mapprox(data, sigma)
+    df = ndapprox(data, h)
+    fm = mapprox(data, h)
     line = np.linspace(start=min(data), stop=max(data), num=iter)
     for t in line:
         res = sco.minimize(fm, [t], method="CG", jac=df)
@@ -60,16 +60,17 @@ def remove_dup(duplicate, x_tol = 0.00001):
             final_list.append(num) 
     return final_list     
     
-df = pd.read_csv("../data/ph/2CFY_out.txt")
+df = pd.read_csv("../data/ph/5DA8_out.txt")
 data = df.x
+print(((4*(np.std(data)**5))/(3*len(data)))**(1/5))
+print(1.06*np.std(data)*(len(data))**(-1/5))
+h = 1.06*np.std(data)*(len(data))**(-1/5)
+
 x = np.linspace(start=min(data), stop=max(data), num=100)
-sigma = 0.001
-f = approx(data, sigma)
+f = approx(data, h)
 g_both = [f(e) for e in x]
 plt.plot(x, g_both, label='silverman')
 plt.legend()
 
-print(minimize(data, sigma))
-
-#print(minimize(fm, df, 0, 1)) 
+print(minimize(data, h))
 
